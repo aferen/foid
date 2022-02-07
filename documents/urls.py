e7939a2ebea42337e80c2bf9e121fd4d2d0781b8
aferen <ahmetfurkanerenn@gmail.com>
2022-02-07 03:52:23 +0300

    added search history crud operations

--- documents/urls.py
+++ documents/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name='documents_index'),
-  path('search', views.search, name='documents_search'),  
+  path('search', views.search, name='documents_search'), 
+  path('search/<int:document_id>', views.searchById, name='documents_searchById'),  
   path('detail/<int:document_id>', views.detail, name='documents_detail'),
-  path('delete/<int:document_id>', views.delete, name='documents_delete')
- 
+  path('delete/<int:document_id>', views.delete, name='documents_delete'),
+  path('deleteHistory/<int:history_id>', views.deleteSearchHistory, name='documents_deleteSearchHistory')
 ]
--- documents/views.py
+++ documents/views.py
@@ -1,6 +1,7 @@
 import itertools
 from functools import reduce
 import operator
+from pydoc import doc
 
 from tika import parser
 from django.shortcuts import redirect
@@ -25,7 +26,7 @@ def index(request):
     documents = Documents.objects.all().order_by('-id')
 
   for doc in documents:
-    searchHistory = SearchHistory.objects.filter(Q(document=doc)).order_by('-pk').last()
+    searchHistory = SearchHistory.objects.filter(Q(document=doc)).order_by('pk').last()
     doc.query = searchHistory.query
     doc.modified_date = searchHistory.modified_date
     
@@ -56,8 +57,35 @@ def search(request):
 
       return redirect('documents_index')
 
-  context = {'form': form}
+  context = {'document': form}
   return render(request, 'documents/search.html', context)
+
+@login_required
+def searchById(request, document_id):
+  user = request.user
+  form = DocumentsForm(user=user)
+  if not user.is_admin:
+    document = Documents.objects.get(Q(id=document_id), Q(user=user))
+  else:
+    document = Documents.objects.get(Q(id=document_id))
+  form.id = document.id
+  form.path = document.id
+  form.docID = document.docID
+  if request.method == 'POST':
+    form = DocumentsForm(request.POST, request.FILES, user=user)
+    if form.is_valid():
+      query = form.data.get('query')
+      SearchHistory.objects.create(document=document, query=query)
+
+      # file_path = t_obj.thesis_doc.field.storage.base_location
+      #file_path = t_obj.documents_doc.file.name
+      #path, file_name = os.path.split(file_path)
+      #Documents.objects.create()
+
+      return redirect('documents_detail', document_id)
+
+  context = {'document': form, 'document_id': form.id}
+  return render(request, 'documents/searchById.html', context)
 
 @login_required
 def detail(request, document_id):
@@ -72,10 +100,29 @@ def detail(request, document_id):
 
 @login_required
 def delete(request, document_id):
-  document = Documents.objects.get(id=document_id)
+  user = request.user
+  if not user.is_admin:
+    document = Documents.objects.get(Q(id=document_id), Q(user=user))
+  else:
+    document = Documents.objects.get(Q(id=document_id))
   if request.method == "POST":
       document.delete()
       return redirect('documents_index')
 
   context = {'document':document}
   return render(request, 'documents/delete.html', context)
+
+@login_required
+def deleteSearchHistory(request, history_id):
+  user = request.user
+  if not user.is_admin:
+    history = SearchHistory.objects.get(Q(id=history_id), Q(user=user))
+  else:
+    history = SearchHistory.objects.get(Q(id=history_id))
+  if request.method == "POST":
+      history.delete()
+      messages.success(request, 'Kayıt Başarılı Bir Şekilde Silindi.')
+      return redirect('documents_detail', history.document.id)
+
+  context = {'history':history}
+  return render(request, 'documents/deleteSearchHistory.html', context)
