40cd9a02511532a1d25bad5f92d31e2904072faf
aferen <ahmetfurkanerenn@gmail.com>
2022-03-05 22:16:40 +0300

    remove drawBox

--- engine/search/detection.py
+++ engine/search/detection.py
@@ -5,6 +5,9 @@ from detectron2.utils.visualizer import ColorMode, Visualizer
 from detectron2.data import MetadataCatalog
 import cv2
 import torch
+import os.path
+import random
+
 
 
 class ObjectDetection(object):
@@ -16,8 +19,8 @@ class ObjectDetection(object):
         self.instance_mode = instance_mode
         self.predictor = DefaultPredictor(cfg)
 
-    def detect(self,path):
-        im = cv2.imread(path)
+    def detect(self,docPath):
+        im = cv2.imread(docPath)
         predictions = self.predictor(im)
         return predictions,self.metadata.thing_classes
         # v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1)
@@ -29,3 +32,16 @@ class ObjectDetection(object):
         #cv2.imshow('Picture', v.get_image()[:, :, ::-1])
         #cv2.waitKey(0)
 
+    # def drawBox(self,docId, imgId, box):
+    #     imgPath = "%s/%s/%s/%s.%s" % ("media/output",docId,"images",imgId, "jpg")
+    #     outputImgPath = "%s/%s/%s/%s.%s" % ("media/tempOutput",docId,"images",imgId, "jpg")
+    #     #color = list(np.random.random(size=3) * 256)
+    #     rgb = (random.random(), random.random(), random.random())
+
+    #     if os.path.isfile(outputImgPath):
+    #         imgPath = outputImgPath
+
+    #     im = cv2.imread(imgPath)
+    #     v = Visualizer(im[:, :, ::-1], self.metadata, scale=1)
+    #     v = v.draw_box(box,edge_color='r')
+    #     cv2.imwrite(outputImgPath, v.get_image()[:, :, ::-1])
