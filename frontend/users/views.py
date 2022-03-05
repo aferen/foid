f7322d29db7ee98c200f1efd69742ea8c71b0a9b
aferen <ahmetfurkanerenn@gmail.com>
2022-03-06 01:39:52 +0300

    merged login and register pages

--- frontend/users/views.py
+++ frontend/users/views.py
@@ -30,7 +30,6 @@ def login(request):
   if request.method == 'POST':
     username = request.POST['username']
     password = request.POST['password']
-
     user = auth.authenticate(username=username, password=password)
 
     if user is not None:
