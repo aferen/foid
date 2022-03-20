1814a34a4a38c6689edff61bcb8aaa4c9beac142
aferen <ahmetfurkanerenn@gmail.com>
2022-03-20 15:11:04 +0300

    added fontawesome package

--- frontend/frontend/settings.py
+++ frontend/frontend/settings.py
@@ -14,7 +14,6 @@ INSTALLED_APPS = [
     'pages',
     'users',
     'documents',
-    'fontawesome-free',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
